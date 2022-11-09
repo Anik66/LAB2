a7b52ad89d6471aa06c672edd2a196082da6fe27
Anik66 <110911348+Anik66@users.noreply.github.com>
2022-11-09 00:15:16 -0800

    Update LoginResult.php

--- LoginResult.php
+++ LoginResult.php
@@ -1,54 +1,27 @@
+
 <?php
-		session_start();
 
-		$uname = $password = "";
-		$unameErr = $unameErr = "";
-		$isValid = true;
+if(isset($_REQUEST["submit"])){
+  if(isset($_REQUEST["name"])||(isset($_REQUEST["password"])))
+    {
 
-		if (isset($_SESSION['uname'])) {		
-			header("location: /LABTASK/view/login.php");
-		}
+      $logData = file_get_contents("../Data/data.json");
+      $logindata = json_decode($logData);
 
-		if ($_SERVER["REQUEST_METHOD"] == "POST") {
-			
-			if (!isset($_POST['uname']) || empty($_POST['uname'])) {
-				$unameErr = "User Name is required";
-				$isValid = false;
-			}
-			else{
-				$username = $_POST['uname'];
-			}
-
-			if (!isset($_POST['pname']) || empty($_POST['pname'])) {
-				$unameErr = "Password is required";
-				$isValid = false;
-			}
-			else{
-				$password = $_POST['pname'];
-			}
-		
-			if($isValid) {
-				$data = json_decode(file_get_contents('../control/data.json'), true);
-
-				if(is_array($data)){
-					$message = "User not found";
-
-					foreach($data as $key => $value){
-						if ($value['uname'] == $_POST['uname']) {
-							if ($value['pname'] == $_POST['pname']) {
-								$_SESSION['data'] = $value;
-								$_SESSION['uname'] = $uname;
-								header("location: /LABTASK/view/login.php");
-							}
-							else{
-								$message = "Password does not match";
-							}
-						}
-					}
-				}
-				else{
-					$message = "User not found";
-				}
-			}
-		}
-	?>
+      foreach($logindata as $myjsonobject)
+      {
+        if($myjsonobject->Username==$_REQUEST["name"] && ($myjsonobject->Password == $_REQUEST["password"]))
+        { 
+          echo "You have Successfully logged in";
+          return;
+        }
+        }
+        echo "Error in username or Password. Please try again.";
+      }
+      
+    }
+    
+      
+     
+    
+?>
